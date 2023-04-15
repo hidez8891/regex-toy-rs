a4ce16fd62e415f2eaf341f48d6a061ef262dc64
hidez8891 <427357+hidez8891@users.noreply.github.com>
2023-04-15 17:29:08 +0900

    WIP: add DFA Matcher

---
+++ src/sfa/dfa/dfa.rs
@@ -0,0 +1,69 @@
+use std::collections::{BTreeSet, HashMap};
+
+use crate::sfa::nfa::nfa::Nfa;
+
+use super::{builder::Builder, matcher::Matcher};
+
+pub type IndexSet = BTreeSet<usize>;
+
+pub struct Dfa {
+    pub nodes: Vec<Node>,
+    pub nodemap: HashMap<IndexSet, usize>,
+}
+
+impl Dfa {
+    pub fn new(pattern: &str) -> Result<Dfa, String> {
+        let nfa = Nfa::new(pattern)?;
+        let dfa = Builder::build(&nfa);
+
+        Ok(dfa)
+    }
+
+    pub fn is_match<'a>(&self, str: &'a str) -> Option<&'a str> {
+        let mut matcher = Matcher::new(&self);
+        matcher.execute(str)
+    }
+}
+
+pub struct Node {
+    pub trans: Transition,
+    pub is_match: bool,
+}
+
+impl Node {
+    pub fn new() -> Self {
+        Node {
+            trans: Transition::new(256),
+            is_match: false,
+        }
+    }
+}
+
+pub struct Transition {
+    pub trans: Vec<IndexSet>,
+    pub start_line: IndexSet,
+    pub end_line: IndexSet,
+}
+
+impl Transition {
+    pub fn new(size: usize) -> Self {
+        let mut trans = Vec::with_capacity(size);
+        for _ in 0..size {
+            trans.push(IndexSet::default());
+        }
+        Transition {
+            trans,
+            start_line: IndexSet::default(),
+            end_line: IndexSet::default(),
+        }
+    }
+
+    pub fn merge(&mut self, other: &Transition) {
+        assert_eq!(self.trans.len(), other.trans.len());
+        for i in 0..self.trans.len() {
+            self.trans[i] = &self.trans[i] | &other.trans[i];
+        }
+        self.start_line = &self.start_line | &other.start_line;
+        self.end_line = &self.end_line | &other.end_line;
+    }
+}
---
+++ src/sfa/dfa/matcher.rs
@@ -0,0 +1,77 @@
+use super::dfa::{Dfa, IndexSet};
+
+pub struct Matcher<'a> {
+    dfa: &'a Dfa,
+    start_index: i32,
+    last_index: i32,
+}
+
+impl<'a> Matcher<'a> {
+    pub fn new(dfa: &'a Dfa) -> Self {
+        Matcher {
+            dfa,
+            start_index: -1,
+            last_index: -1,
+        }
+    }
+
+    fn reset(&mut self) {
+        *self = Matcher {
+            dfa: self.dfa,
+            start_index: -1,
+            last_index: -1,
+        }
+    }
+
+    pub fn execute<'b>(&mut self, str: &'b str) -> Option<&'b str> {
+        for i in 0..str.len() {
+            self.reset();
+
+            let result = self.execute_(str, i);
+            if result.is_some() {
+                return result;
+            }
+        }
+        None
+    }
+
+    fn execute_<'b>(&mut self, str: &'b str, sp: usize) -> Option<&'b str> {
+        self.start_index = sp as i32;
+
+        let mut index = self.dfa.nodemap.iter().find(|v| *v.1 == 0).unwrap().0;
+        let mut sp = sp;
+
+        while !index.is_empty() {
+            let id = self.dfa.nodemap[index];
+            let node = &self.dfa.nodes[id];
+
+            if node.is_match {
+                self.last_index = sp as i32;
+            }
+
+            if sp == 0 && !node.trans.start_line.is_empty() {
+                index = &node.trans.start_line;
+                continue;
+            }
+            if sp == str.len() && !node.trans.end_line.is_empty() {
+                index = &node.trans.end_line;
+                continue;
+            }
+            if sp >= str.len() {
+                break; // end while loop
+            }
+
+            let c = str.chars().nth(sp).unwrap();
+            index = &node.trans.trans[c as usize];
+            sp += 1;
+        }
+
+        if self.start_index <= self.last_index {
+            let s = self.start_index as usize;
+            let e = self.last_index as usize;
+            Some(&str[s..e])
+        } else {
+            None
+        }
+    }
+}
