ff7ae5005921f7109955dae5b930ac585c7e3037
hidez8891 <427357+hidez8891@users.noreply.github.com>
2023-04-21 23:55:07 +0900

    WIP: VM matcher supports captures

--- src/vm/compile.rs
+++ src/vm/compile.rs
@@ -4,47 +4,67 @@ use crate::parser::{
     Ast,
 };
 
-pub(crate) struct Compiler {}
+pub(crate) struct Compiler {
+    max_capture_id: usize,
+}
 
 impl Compiler {
-    pub fn compile(ast: &Ast) -> Vec<Inst> {
-        let mut insts = Self::compile_root(ast);
+    pub fn compile(ast: &Ast) -> (Vec<Inst>, usize) {
+        let mut compiler = Compiler { max_capture_id: 0 };
+
+        let mut insts = compiler.compile_root(ast);
+        insts.insert(0, Inst::CaptureStart(0));
+        insts.push(Inst::CaptureEnd(0));
         insts.push(Inst::Success);
-        insts
+
+        (insts, compiler.max_capture_id + 1)
     }
 
-    fn compile_root(ast: &Ast) -> Vec<Inst> {
+    fn compile_root(&mut self, ast: &Ast) -> Vec<Inst> {
         match &ast.kind {
-            AstKind::CaptureGroup(_) => Self::compile_group(ast),
-            AstKind::NonCaptureGroup => Self::compile_group(ast),
-            AstKind::Union => Self::compile_union(ast),
-            AstKind::IncludeSet => Self::compile_include_set(ast),
-            AstKind::ExcludeSet => Self::compile_exclude_set(ast),
-            AstKind::Star(greedy) => Self::compile_star(ast, greedy),
-            AstKind::Plus(greedy) => Self::compile_plus(ast, greedy),
-            AstKind::Option(greedy) => Self::compile_option(ast, greedy),
-            AstKind::Repeat(n, m, greedy) => Self::compile_repeat(ast, n, m, greedy),
+            AstKind::CaptureGroup(cap_id) => {
+                if self.max_capture_id < *cap_id {
+                    self.max_capture_id = *cap_id;
+                }
+                self.compile_group(ast, *cap_id)
+            }
+            AstKind::NonCaptureGroup => self.compile_group(ast, 0),
+            AstKind::Union => self.compile_union(ast),
+            AstKind::IncludeSet => self.compile_include_set(ast),
+            AstKind::ExcludeSet => self.compile_exclude_set(ast),
+            AstKind::Star(greedy) => self.compile_star(ast, greedy),
+            AstKind::Plus(greedy) => self.compile_plus(ast, greedy),
+            AstKind::Option(greedy) => self.compile_option(ast, greedy),
+            AstKind::Repeat(n, m, greedy) => self.compile_repeat(ast, n, m, greedy),
             AstKind::Match(kind) => Self::compile_match(kind),
             AstKind::Position(kind) => Self::compile_position(kind),
         }
     }
 
-    fn compile_group(ast: &Ast) -> Vec<Inst> {
+    fn compile_group(&mut self, ast: &Ast, cap_id: usize) -> Vec<Inst> {
         let mut insts = Vec::new();
-        for child in ast.children.iter() {
-            insts.extend(Self::compile_root(child));
+
+        if cap_id > 0 {
+            insts.push(Inst::CaptureStart(cap_id))
         }
+        for child in ast.children.iter() {
+            insts.extend(self.compile_root(child));
+        }
+        if cap_id > 0 {
+            insts.push(Inst::CaptureEnd(cap_id))
+        }
+
         insts
     }
 
-    fn compile_union(ast: &Ast) -> Vec<Inst> {
+    fn compile_union(&mut self, ast: &Ast) -> Vec<Inst> {
         let mut insts = Vec::new();
 
         let mut dst_addr = 2;
         insts.push(Inst::Fail);
 
         for child in ast.children.iter().rev() {
-            let mut child_insts = Self::compile_root(child);
+            let mut child_insts = self.compile_root(child);
             child_insts.reverse();
 
             let next_addr = child_insts.len() as isize + 2;
@@ -60,7 +80,7 @@ impl Compiler {
         insts
     }
 
-    fn compile_include_set(ast: &Ast) -> Vec<Inst> {
+    fn compile_include_set(&mut self, ast: &Ast) -> Vec<Inst> {
         let mut insts = Vec::new();
         insts.push(Inst::Seek(1));
         insts.push(Inst::Fail);
@@ -90,7 +110,7 @@ impl Compiler {
         insts
     }
 
-    fn compile_exclude_set(ast: &Ast) -> Vec<Inst> {
+    fn compile_exclude_set(&mut self, ast: &Ast) -> Vec<Inst> {
         let mut insts = Vec::new();
         insts.push(Inst::Fail);
         insts.push(Inst::Jmp(2));
@@ -121,8 +141,8 @@ impl Compiler {
         insts
     }
 
-    fn compile_star(ast: &Ast, greedy: &GreedyKind) -> Vec<Inst> {
-        let child_insts = Self::compile_root(&ast.children[0]);
+    fn compile_star(&mut self, ast: &Ast, greedy: &GreedyKind) -> Vec<Inst> {
+        let child_insts = self.compile_root(&ast.children[0]);
         let child_size = child_insts.len() as isize;
 
         let mut insts = Vec::new();
@@ -137,8 +157,8 @@ impl Compiler {
         insts
     }
 
-    fn compile_plus(ast: &Ast, greedy: &GreedyKind) -> Vec<Inst> {
-        let child_insts = Self::compile_root(&ast.children[0]);
+    fn compile_plus(&mut self, ast: &Ast, greedy: &GreedyKind) -> Vec<Inst> {
+        let child_insts = self.compile_root(&ast.children[0]);
         let child_size = child_insts.len() as isize;
 
         let mut insts = Vec::new();
@@ -152,8 +172,8 @@ impl Compiler {
         insts
     }
 
-    fn compile_option(ast: &Ast, greedy: &GreedyKind) -> Vec<Inst> {
-        let child_insts = Self::compile_root(&ast.children[0]);
+    fn compile_option(&mut self, ast: &Ast, greedy: &GreedyKind) -> Vec<Inst> {
+        let child_insts = self.compile_root(&ast.children[0]);
         let child_size = child_insts.len() as isize;
 
         let mut insts = Vec::new();
@@ -168,7 +188,7 @@ impl Compiler {
     }
 
     #[rustfmt::skip]
-    fn compile_repeat(
+    fn compile_repeat(&mut self,
         ast: &Ast,
         min: &RepeatKind,
         max: &RepeatKind,
@@ -176,13 +196,13 @@ impl Compiler {
     ) -> Vec<Inst> {
         match (min, max) {
             (RepeatKind::Num(n), RepeatKind::Num(m)) if n == m => {
-                Self::compile_repeat_count(ast, *n)
+                self.compile_repeat_count(ast, *n)
             }
             (RepeatKind::Num(n), RepeatKind::Num(m)) => {
-                Self::compile_repeat_range(ast, *n, *m, greedy)
+                self.compile_repeat_range(ast, *n, *m, greedy)
             }
             (RepeatKind::Num(c), RepeatKind::Infinity) => {
-                Self::compile_repeat_min(ast, *c, greedy)
+                self.compile_repeat_min(ast, *c, greedy)
             }
             (RepeatKind::Infinity, _) => {
                 unreachable!()
@@ -190,8 +210,8 @@ impl Compiler {
         }
     }
 
-    fn compile_repeat_count(ast: &Ast, count: u32) -> Vec<Inst> {
-        let child_insts = Self::compile_root(&ast.children[0]);
+    fn compile_repeat_count(&mut self, ast: &Ast, count: u32) -> Vec<Inst> {
+        let child_insts = self.compile_root(&ast.children[0]);
 
         let mut insts = Vec::new();
         for _ in 0..count {
@@ -200,15 +220,21 @@ impl Compiler {
         insts
     }
 
-    fn compile_repeat_min(ast: &Ast, count: u32, greedy: &GreedyKind) -> Vec<Inst> {
+    fn compile_repeat_min(&mut self, ast: &Ast, count: u32, greedy: &GreedyKind) -> Vec<Inst> {
         let mut insts = Vec::new();
-        insts.extend(Self::compile_repeat_count(ast, count));
-        insts.extend(Self::compile_star(ast, greedy));
+        insts.extend(self.compile_repeat_count(ast, count));
+        insts.extend(self.compile_star(ast, greedy));
         insts
     }
 
-    fn compile_repeat_range(ast: &Ast, min: u32, max: u32, greedy: &GreedyKind) -> Vec<Inst> {
-        let mut child_insts = Self::compile_root(&ast.children[0]);
+    fn compile_repeat_range(
+        &mut self,
+        ast: &Ast,
+        min: u32,
+        max: u32,
+        greedy: &GreedyKind,
+    ) -> Vec<Inst> {
+        let mut child_insts = self.compile_root(&ast.children[0]);
         child_insts.reverse();
 
         let mut insts = Vec::new();
@@ -224,7 +250,7 @@ impl Compiler {
             }
         }
 
-        let mut repeat_insts = Self::compile_repeat_count(ast, min);
+        let mut repeat_insts = self.compile_repeat_count(ast, min);
         repeat_insts.reverse();
         insts.extend(repeat_insts);
 
